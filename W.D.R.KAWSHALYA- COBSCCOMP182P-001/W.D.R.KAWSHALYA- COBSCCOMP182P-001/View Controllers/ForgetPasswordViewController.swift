2d44886357c221caebde2e9f4d94f980451cb491
ravishankawshalya20 <cobsccomp182p-001@student.nibm.lk>
2020-02-29 23:47:31 +0530

    Ravishan Commit

---
+++ W.D.R.KAWSHALYA- COBSCCOMP182P-001/W.D.R.KAWSHALYA- COBSCCOMP182P-001/View Controllers/ForgetPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ForgetPasswordViewController.swift
+//  W.D.R.KAWSHALYA- COBSCCOMP182P-001
+//
+//  Created by Ravishan Kawshalya on 29/02/2020.
+//  Copyright © 2020 Ravishan Kawshalya. All rights reserved.
+//
+
+import UIKit
+
+class ForgetPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ W.D.R.KAWSHALYA- COBSCCOMP182P-001/W.D.R.KAWSHALYA- COBSCCOMP182P-001/View Controllers/RegisterViewController.swift
@@ -0,0 +1,55 @@
+//
+//  RegisterViewController.swift
+//  W.D.R.KAWSHALYA- COBSCCOMP182P-001
+//
+//  Created by Ravishan Kawshalya on 29/02/2020.
+//  Copyright © 2020 Ravishan Kawshalya. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+   
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var firstNameTxt: UITextField!
+    
+    @IBOutlet weak var confirmPasswordTxt: UITextField!
+    @IBOutlet weak var lastNameTxt: UITextField!
+    
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var phoneNumberTxt: UITextField!
+    
+    @IBAction func addButton(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func registerButton(_ sender: Any) {
+    }
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
